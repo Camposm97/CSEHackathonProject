b9658b8b364b8204f04f5cadedefe325d806c681
Robert Cabrera <robertcabrera4871@gmail.com>
2019-04-22 21:54:57 -0400

    test

--- cabreraSrc/cabrera/util/CheckUserNameUtil.java
+++ cabreraSrc/cabrera/util/CheckUserNameUtil.java
@@ -8,12 +8,18 @@ public class CheckUserNameUtil {
 	public static ObservableList<String> filterList(ObservableList<String> temp, String input) {
 		ObservableList<String> newList = FXCollections.observableArrayList();
 		temp.forEach((String) -> {
-				String testList = String.substring(0, input.length()).toUpperCase();
-				String testInput = input.toUpperCase();
-				
-				if (testInput.equals(testList)) {
-					newList.add(String);
-				}
+			String testList = null;
+			String testInput = null;
+			//boolean isNull = (testList == null && testInput == null);
+			if (!(input.length() > String.length())) {
+				testList = String.substring(0, input.length()).toUpperCase();
+				testInput = input.toUpperCase();
+			}
+			System.out.println(testInput + " testInput");
+			System.out.println(testList + " testList");
+			if (testInput.equals(testList) && testList != null) {
+				newList.add(String);
+			}
 
 		});
 		return newList;
