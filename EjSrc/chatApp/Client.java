557f91d34cf49051a72b981700e8958c7e8e69cf
Camposm97 <camposm1205@gmail.com>
2019-04-23 21:38:12 -0400

    Merging

--- EjSrc/chatApp/Client.java
+++ EjSrc/chatApp/Client.java
@@ -66,6 +66,6 @@ public class Client
   
     public static void main(String args[]) 
     { 
-        Client client = new Client("127.0.0.1", 5000); 
+        Client client = new Client("225.100.1.1", 65535); 
     } 
 } 
