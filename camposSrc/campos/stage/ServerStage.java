11677af523069ac4cd0432e5eb7e222117ebdeaa
Camposm97 <camposm1205@gmail.com>
2019-04-24 20:48:34 -0400

    Resolving Ej's GroupChat.java Merge Conflict (We should probably learn how to deal with merge conflicts properly for the future...)

--- camposSrc/campos/stage/ServerStage.java
+++ camposSrc/campos/stage/ServerStage.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import campos.io.DataLoader;
 import campos.models.UserAccountBag;
 import campos.scene.layout.ServerPane;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class ServerStage extends Stage {
 	private UserAccountBag userBag;	// Change to Tree, go faster O(log(N))
@@ -16,17 +18,24 @@ public class ServerStage extends Stage {
 		this.userBag = DataLoader.loadUsers();
 		this.root = new ServerPane(port);
 		this.setScene(new Scene(root));
-		this.setOnCloseRequest(e -> {
-			try {
-				System.out.println("Closing server...");
-				root.getServer().close();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-		});
+		this.setOnCloseRequest(new ServerCloseHandler());
+		this.setWidth(512);
 	}
 	
 	public UserAccountBag getUserBag() {
 		return userBag;
+	}
+	
+	private class ServerCloseHandler implements EventHandler<WindowEvent> {
+
+		@Override
+		public void handle(WindowEvent event) {
+			try {
+				root.getServer().close();
+				System.out.println("Closing server...");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
