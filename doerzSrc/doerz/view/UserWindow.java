f567f29f95da77820d170770e8c31c3f5deb282d
rjdoerz <43040953+rjdoerz@users.noreply.github.com>
2019-05-07 21:25:17 -0400

    Repurposed as "Edit Profile." Implements AvatarPicker(proof of concept)

--- doerzSrc/doerz/view/UserWindow.java
+++ doerzSrc/doerz/view/UserWindow.java
@@ -5,43 +5,57 @@ import campos.models.Major;
 import campos.models.Name;
 import campos.models.Student;
 import campos.models.UserAccount;
+import campos.scene.layout.AvatarPicker;
+import campos.util.ImgUtil;
+import doerz.view.layout.ProfilePane;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import util.Dummy;
 
 /*
- * Temporary way to create a user account to post with. This is used for development purposes only.
+ * Quick way to edit a userAccounts information
  */
 
 public class UserWindow {
-	private Button okBtn;
+	private Button okBtn, avatarBtn;
+	private ImageView avatar;
 	private TextField firstFld, lastFld, userFld;
 	private	Stage userStage;
 	
 	public UserWindow(UserAccount user) {
 		VBox box = new VBox();
 		box.setPadding(new Insets(20));
+		box.setSpacing(5);
 		
 		userStage = new Stage();
-		Scene userScene = new Scene(box, 200, 300);
+		userStage.setResizable(false);
+		
+		Scene userScene = new Scene(box, 210, 360);
 			firstFld = new TextField();
 			lastFld = new TextField();
-			userFld = new TextField();
+			userFld = new TextField(user.getUsername());
 		
 			okBtn = new Button("Okay");
 			okBtn.setAlignment(Pos.CENTER);
 			
-		Label firstLbl, lastLbl, userLbl;
+			avatar = ImgUtil.loadImgVR(ImgUtil.DEFAULT_PROFILE, 0.4);
+			avatarBtn = new Button();
+			avatarBtn.setGraphic(avatar);
+			avatarBtn.setAlignment(Pos.CENTER);
+			
+		Label avatarLbl, firstLbl, lastLbl, userLbl;
+			avatarLbl = new Label("Avatar:");
 			firstLbl = new Label("First Name:");
 			lastLbl = new Label("Last Name:");
 			userLbl = new Label("Username:");
-		box.getChildren().addAll(firstLbl, firstFld, lastLbl, lastFld, userLbl, userFld, okBtn);
+		box.getChildren().addAll(avatarLbl, avatarBtn, firstLbl, firstFld, lastLbl, lastFld, userLbl, userFld, okBtn);
 		
 		callBacks(user);
 		
@@ -65,8 +79,13 @@ public class UserWindow {
 			userAcc.setPassword(Dummy.getRandomPass());
 			
 //			userAcc = new UserAccount(student, user, Dummy.getRandomPass());
-			
+			ProfilePane.refreshNodes();
 			userStage.close();
+		});
+		
+		avatarBtn.setOnAction(e -> {
+			AvatarPicker picker = new AvatarPicker();
+			picker.showAndWait();
 		});
 	}
 }
