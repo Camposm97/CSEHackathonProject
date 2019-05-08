c6d53a7e7d9b1d4abdb54fef8c5f72850e62fd1e
rjdoerz <43040953+rjdoerz@users.noreply.github.com>
2019-05-07 21:31:50 -0400

    Set Title. Changed: implementation of profilePane, Feed from folowedUsrs

--- doerzSrc/doerz/view/MainWindow.java
+++ doerzSrc/doerz/view/MainWindow.java
@@ -3,6 +3,7 @@ package doerz.view;
 import java.util.LinkedList;
 
 import campos.models.UserAccount;
+import doerz.collection.FollowedFeed;
 import doerz.model.Post;
 import doerz.view.layout.ComposePane;
 import doerz.view.layout.FeedPane;
@@ -18,22 +19,26 @@ public class MainWindow {
 	 * MainWindow is the hub that controls what is displayed as the home screen.
 	 * After being successfully logged in, users should be sent here. 
 	 * 
-	 * Pass in the app's primary stage as well as the successful login account.
+	 * Pass in the successful login account.
 	 */
 
-	public MainWindow(Stage stage, UserAccount user) {
+	public MainWindow(UserAccount user) {
 		BorderPane root = new BorderPane();
 		BorderPane nest = new BorderPane();
 		root.setCenter(nest);
 		Scene scene = new Scene(root, 950, 850);
+		Stage stage = new Stage();
+		stage.setTitle("Connect");
 		stage.setResizable(false);
 		stage.setScene(scene);
 		stage.show();
 		
-		LinkedList<Post> feed = new LinkedList<Post>();
+//		LinkedList<Post> feed = user.getPostList();
+		LinkedList<Post> feed = FollowedFeed.getFeedPosts(user);
+
 		nest.setCenter(new ComposePane(user).getNode());
 		nest.setBottom(new FeedPane(feed).getNode());
-		new ProfilePane(root);
+		root.setLeft(new ProfilePane(user).getNode());
 		new HeaderPane(root, stage);
 		
 	}
